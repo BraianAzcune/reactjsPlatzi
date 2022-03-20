5c6c876225ae1e2bcf8ea397876851174b26f1ef
BraianAzcune <braian.azcune@cedi.com.ar>
2022-03-20 20:14:42 -0300

    UseState tiene 3 modos, todo de forma imperativa

--- src/App/UseState.js
+++ src/App/UseState.js
@@ -7,38 +7,76 @@ export function UseState({ name }) {
   const [state, setState] = React.useState({
     error: false,
     loading: false,
-    value: ''
+    value: '',
+    deleted: false,
+    confirmed: false
   });
 
   React.useEffect(() => {
     if (state.loading) {
       setTimeout(() => {
-        setState({ ...state, loading: false, error: state.value === SECURITY_CODE ? false : true });
+        const correct_password = state.value === SECURITY_CODE ? true : false;
+        setState({
+          ...state,
+          value: '',
+          loading: false,
+          error: !correct_password,
+          confirmed: correct_password,
+        });
         ;
       }, 1000);
     }
   }, [state.loading]);
 
-  return (
-    <div>
-      <h2>Eliminar UseState</h2>
-      <p>{name}</p>
-      {
-        (state.error && !state.loading) && <p>Error en el codigo de seguridad</p>
-      }
-      {
-        state.loading && <p>Cargando...</p>
-      }
-      <p>Por favor escribe el codigo de seguridad</p>
-      <input
-        value={state.value}
-        onChange={(event) => {
-          setState({ ...state, value: event.target.value });
-        }} placeholder='Codigo de seguridad'></input>
-      <button
-        disabled={state.loading}
-        onClick={() => { setState({ ...state, loading: true }); }}>
-        Comprobar</button>
-    </div>
-  );
+
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar UseState</h2>
+        <p>{name}</p>
+        {
+          (state.error && !state.loading) && <p>Error en el codigo de seguridad</p>
+        }
+        {
+          state.loading && <p>Cargando...</p>
+        }
+        <p>Por favor escribe el codigo de seguridad</p>
+        <input
+          value={state.value}
+          onChange={(event) => {
+            setState({ ...state, value: event.target.value });
+          }} placeholder='Codigo de seguridad'></input>
+        <button
+          disabled={state.loading}
+          onClick={() => { setState({ ...state, loading: true }); }}>
+          Comprobar</button>
+      </div>
+    );
+  } else if (!state.deleted && state.confirmed) {
+    return (
+      <>
+        <p>¿ Estas seguro ?</p>
+        <button
+          onClick={() => { setState({ ...state, deleted: true }) }}
+        >
+          Si
+        </button>
+        <button
+          onClick={() => { setState({ ...state, confirmed: false }) }}
+        >
+          No
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>Eliminado con exito</p>
+        <button
+          onClick={() => { setState({ ...state, deleted: false, confirmed: false }) }}
+        >volver</button>
+      </>
+    );
+  }
+
 }
